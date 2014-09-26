7c1d8ce9e7e175b5a4ca12cedb2ed5ed25d86eca
kevin.smith <kevin.smith@sequensis.co.uk>
2014-09-26 15:27:48 +0100

    init commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+    // Countdown
+
+    var date = new Date(config.countdown.year,
+                        config.countdown.month - 1,
+                        config.countdown.day,
+                        config.countdown.hour,
+                        config.countdown.minute,
+                        config.countdown.second),
+        $countdownNumbers = {
+            days: $('#countdown-days'),
+            hours: $('#countdown-hours'),
+            minutes: $('#countdown-minutes'),
+            seconds: $('#countdown-seconds')
+        };
+
+    $('#countdown').countdown(date).on('update.countdown', function(event) {
+        $countdownNumbers.days.text(event.offset.totalDays);
+        $countdownNumbers.hours.text(('0' + event.offset.hours).slice(-2));
+        $countdownNumbers.minutes.text(('0' + event.offset.minutes).slice(-2));
+        $countdownNumbers.seconds.text(('0' + event.offset.seconds).slice(-2));
+    });
