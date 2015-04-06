0796131c268d3a67ced06db3d00c1e1a3039ae7d
kevbite <kev_bite@msn.com>
2015-04-06 20:05:00 +0100

    timer

--- js/main.config.js
+++ js/main.config.js
@@ -1,8 +1,8 @@
 var config = {
     countdown: {
         year: 2015,
-        month: 3,
-        day: 3,
+        month: 4,
+        day: 7,
         hour: 18,
         minute: 30,
         second: 0
