7517898897b48e6cf754f3313b626b62e7d2afab
Kevin Smith <kev_bite@msn.com>
2015-05-05 15:39:05 +0100

    updating timer config.

--- js/main.config.js
+++ js/main.config.js
@@ -1,8 +1,8 @@
 var config = {
     countdown: {
         year: 2015,
-        month: 5,
-        day: 5,
+        month: 6,
+        day: 2,
         hour: 18,
         minute: 30,
         second: 0
