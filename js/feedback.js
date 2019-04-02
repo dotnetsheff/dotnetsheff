7c1097297048329cb8d9a2439a162bc549ed8463
Sakis Koltsidas <sakis.koltsidas@bybox.com>
2019-04-02 13:46:05 +0100

    Work to bind talk ratings to form submision data

--- js/feedback.js
+++ js/feedback.js
@@ -9,19 +9,65 @@
         },
         viewModels: {}
     };
+
+    dotnetsheff.viewModels.FeedbackTalkViewModel = function(eventId, title, speaker){
+        this.eventId = eventId;
+        this.title = title;
+        this.speaker = speaker;
+        this.overallRating = ko.observable();
+        this.enjoyAboutTalk = ko.observable();
+        this.improvementsAboutTalk = ko.observable();
+    };
+
+    dotnetsheff.viewModels.FeedbackEventViewModel = function(){
+        this.id = ko.observable();
+        this.title = ko.observable();
+        this.talks = ko.observableArray([]);
+        this.overalRating = ko.observable();
+        this.Overall = ko.observable();
+        this.Food = ko.observable();
+        this.Drinks = ko.observable();
+        this.Venue = ko.observable();
+        this.improvementsAboutEvent = ko.observable();
+    }
+
     dotnetsheff.viewModels.FeedbackEventsViewModel = function () {
         var self = this;
 
         self.events = ko.observableArray();
-        self.selectedTalk = ko.observable();
-        self.id = self.selectedTalk.Title;
-        
+        self.events.Talks = ko.observableArray();
+        self.selectedEvent = ko.observable();
+        self.enjoyAboutEvent = ko.observable();
+        self.Overall = ko.observable();
+        self.Food = ko.observable();
+        self.Drinks = ko.observable();
+        self.Venue = ko.observable();
+        self.improvementsAboutEvent = ko.observable();
+        self.enjoyAboutTalk = ko.observable();
+        self.improvementsAboutTalk = ko.observable();
+
         var fetchFeedbackEvents = function(){
             $.getJSON(dotnetsheff.constants.apiUri + "/feedback/available-events", 
             function(data){
                 self.events(data);
             });
-        }; 
+        };
+        
+        self.sendFeedback = function(formElement)
+        {
+            var viewModel = {};
+            viewModel.enjoyAboutEvent = self.enjoyAboutEvent();
+            viewModel.Overall = self.Overall();
+            viewModel.Food = self.Food();
+            viewModel.Drinks = self.Drinks();
+            viewModel.Venue = self.Venue();
+            viewModel.improvementsAboutEvent = self.improvementsAboutEvent();
+            viewModel.selectedEvent = self.selectedEvent();
+            viewModel.enjoyAboutTalk = self.enjoyAboutTalk();
+            viewModel.improvementsAboutTalk = self.improvementsAboutTalk();
+
+            console.log(ko.toJSON(viewModel));
+        };
 
         (function(){
             fetchFeedbackEvents();
